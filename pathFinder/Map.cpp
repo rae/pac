3843f354a8c9fd400974ac073fc715f258121f63
Reid Ellis <rae@tnir.org>
2017-04-14 23:39:19 -0400

    Now compiiles.. and runs!

--- pathFinder/Map.cpp
+++ pathFinder/Map.cpp
@@ -6,50 +6,46 @@
 //  Copyright © 2017 Tnir Technologies. All rights reserved.
 //
 
+#include <assert.h>
 #include "Map.h"
 #include "Node.h"
 
-/////////////////////////////////////////////////////////////////
-//
-// data looks like this
-//
-const char *map[] = {
-	"#######",
-	"#     #",
-	"#  ####",
-	"#     #",
-	"#######"
-};
-
-const int mapRowCount = sizeof(map) / sizeof(map[0]);
-
-//
-// end of data
-//
-/////////////////////////////////////////////////////////////////
-
-
 void Map::parseMap(const char *strings[], int rowCount)
 {
-	if(mapNodes != nullptr) {
-		clearMap();
-	}
+	// clear out existing map, if any
+	clearMap();
+
 	// sanity checks
 	if(rowCount == 0 || strings == nullptr) {
 		return;
 	}
+
+	//
+	// about the mapNodes triple-pointer..
+	//
+	// each element of the map is a *pointer* to a Node, not a node itself,
+	// so 3 levels of indirection are required.
+	//
+	// Maybe using a 'typedef Node *NodePtr;' would help make it clearer? Not sure.
+	//
+
 	// allocate the array of pointers which will point at the rows
 	mapNodes = new Node**[rowCount];
 
-	// assume all strings are the same width
+	// assume all strings are the same width (verified with assert below)
 	width = (int)strlen(strings[0]);
+	height = rowCount;
 
 	// populate the rows
-	for(int y=0; y<rowCount; y++) {
-		// allocate Nodes and point the row pointer at them
+	for(int y=0; y<height; y++) {
+		// allocate an array of Node pointers and point the row pointer at them
 		mapNodes[y] = new Node*[width];
+		// validate the data
+		assert(strings[y] != nullptr && strlen(strings[y]) == width);
+		// create this row of nodes
 		for(int x=0; x<width; x++) {
 			mapNodes[y][x] = new Node(x, y);
+			// canBePath is true if the string has a space here
 			mapNodes[y][x]->canBePath = (strings[y][x] == ' ');
 		}
 	}
@@ -57,6 +53,9 @@ void Map::parseMap(const char *strings[], int rowCount)
 
 void Map::clearMap()
 {
+	if(mapNodes == nullptr) {
+		return;
+	}
 	for(int i=0; i<height; i++) {
 		delete [] mapNodes[i];
 	}
@@ -64,3 +63,12 @@ void Map::clearMap()
 	mapNodes = nullptr;
 }
 
+Node * Map::nodeAt(int x, int y)
+{
+	Node * node = nullptr;
+	// bounds checking
+	if(x >= 0 && x < width && y >= 0 && y < height) {
+		node = mapNodes[y][x];
+	}
+	return node;
+}
--- pathFinder/main.cpp
+++ pathFinder/main.cpp
@@ -10,8 +10,28 @@
 #include "Node.h"
 #include "Map.h"
 
+const char *mapData[] = {
+	"#######",
+	"#     #",
+	"#  ####",
+	"#     #",
+	"#######"
+};
+
+const int mapRowCount = sizeof(mapData) / sizeof(mapData[0]);
+
 int main(int argc, const char * argv[]) {
 	Map * map = new Map;
-
+	map->parseMap(mapData, mapRowCount);
+	for(int y=0; y<map->height; y++) {
+		for(int x=0; x<map->width; x++) {
+			if(map->nodeAt(x, y)->canBePath) {
+				std::cout << " ";
+			} else {
+				std::cout << "#";
+			}
+		}
+		std::cout << std::endl;
+	}
 	return 0;
 }
